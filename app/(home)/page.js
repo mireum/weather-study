baad66816be1ad94a27a0f9dd12230cf9b03dd4d
fkdkskdfjfk <yljw225@naver.com>
2024-04-17 22:02:56 +0900

    useEffect사용

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import Main from './Main';
 import WeatherData from './WeatherData';
 import dfs_xy_conv from './Function';
+import Link from 'next/link';
 // import WeatherData from './WeatherData';
 
 
@@ -31,6 +32,7 @@ function Home() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [position, setPosition] = useState([]);
+  const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
     const GetLocation = () => {
@@ -56,21 +58,64 @@ function Home() {
     GetLocation();
   }, []);
 
-  return (
-    <div>
-      <h1>사용자 위치 정보</h1>
+  useEffect(() => {
+    if (latitude && longitude) {
+      const getWeather = async () => {
+        try {
+          // https://dayday.devcjw.com
+          const data = await fetch('/api/getWeather', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              lng: `${longitude}`,
+              lat: `${latitude}`,
+            })
+          });
+          const json = await data.json();
+          setWeatherData(json);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      getWeather();
+    }
+  }, [latitude, longitude]);
 
-      {/* <div>{latitude}, {longitude}</div> */}
-      {/* <Middle /> */}
-      {/* {latitude && <GetWeather latitude={latitude} longitude={longitude}  />} */}
-      
-      {/* {<WeatherData />} */}
-      {/* {<GetWeatherData />} */}
-      {longitude && latitude && 
+
+  return (
+    <>
+      <div>
+        <div className="w-4/5 m-auto">
+          <div className="w-3/5 m-auto flex p-5">
+            <Link className="text-sky-600 hover:text-sky-700" href={"/"}>
+              그날그날
+            </Link>
+
+            <div className="flex gap-2 ml-auto">
+              <button className="text-green-600" onClick={undefined}>
+                Sign In
+              </button>
+            </div>
+          </div>
+
+          <div>
+            {weatherData ? (
+              // <WeatherData data={weatherData.response.body.items.item} position={position}/>
+              <div>WeatherData 있음</div>
+            ) : (
+              <p>날씨 정보를 가져오는 중...</p>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* {longitude && latitude && 
       <Main>
         <WeatherData latitude={latitude} longitude={longitude}/>
-      </Main>}
-    </div>
+      </Main>} */}
+    </>
+
   );
 }
 
