b795af4731d57583d8f6f9c9046d41e3324e1475
fkdkskdfjfk <yljw225@naver.com>
2024-04-17 22:44:42 +0900

    SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON

--- app/(home)/Function.js
+++ app/(home)/Function.js
@@ -69,3 +69,20 @@ export default function dfs_xy_conv(code, v1, v2) {
   }
   return rs;
 }
+
+export const getDate = () => {
+    const day = new Date();
+    let nowTime = String(day.getHours())+String(day.getMinutes()).padStart(2, '0');
+    nowTime = Number(nowTime);
+    let standTime;
+    if (nowTime > 2310) {standTime = '2300'}
+    else if (nowTime > 2010) {standTime = '2000'}
+    else if (nowTime > 1710) {standTime = '1700'}
+    else if (nowTime > 1410) {standTime = '1400'}
+    else if (nowTime > 1110) {standTime = '1100'}
+    else if (nowTime > 810) {standTime = '0800'}
+    else if (nowTime > 510) {standTime = '0500'}
+    else {standTime = '0200'}
+    
+    return standTime;
+  }
--- app/api/GetData.js
+++ app/api/GetData.js
@@ -1,3 +1,5 @@
+import { getDate } from "../(home)/Function";
+
 export async function getData(latitude, longitude) {
   try {
     const apiKey = '481EB071-C586-3A91-9DB2-23C53DC4ACA0';
@@ -11,6 +13,26 @@ export async function getData(latitude, longitude) {
     const json = await response.json();
     
     return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getWeather(latitude, longitude) {
+  try {
+    console.log(process.env.WEATHER_API_KEY);
+    const apiKey = process.env.WEATHER_API_KEY;
+    const day = new Date();
+    const baseDate = day.getFullYear()+String(day.getMonth()+1).padStart(2, '0')+String(day.getDate()).padStart(2, '0');
+    const baseTime = getDate();
+    const nx = longitude;
+    const ny = latitude;
+    const apiUrl = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${apiKey}&numOfRows=12&pageNo=1&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json(data);
+    
   } catch (error) {
     console.error(error);
   }
