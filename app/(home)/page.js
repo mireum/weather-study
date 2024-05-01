e0d03ee1fdf558d82bb46b676bfbcf4c675a9002
fkdkskdfjfk <yljw225@naver.com>
2024-05-01 11:13:41 +0900

    1

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -111,10 +111,10 @@ function Home() {
         </div>
       </div>
       
-      {/* {longitude && latitude && 
+      {longitude && latitude && 
       <Main>
         <WeatherData latitude={latitude} longitude={longitude}/>
-      </Main>} */}
+      </Main>}
     </>
 
   );
